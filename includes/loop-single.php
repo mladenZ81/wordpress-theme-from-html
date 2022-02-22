b3aba2558605a9ce9ef241acd55685729b8803d0
mladenZ81 <99220544+mladenZ81@users.noreply.github.com>
2022-02-22 15:37:49 +0100

    Add files via upload

---
+++ includes/loop-single.php
@@ -0,0 +1,19 @@
+<?php while ( have_posts() ) : the_post(); //Open the loop ?>
+
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		
+		<section class="entry-meta">
+			<?php echo get_the_date(); ?>
+		</section><!-- .entry-meta -->
+		
+		<section class="entry-content">
+			<?php the_content(); ?>
+		</section><!-- .entry-content -->	
+
+		<section class="entry-utility">
+			<?php comments_template(); ?>
+		</section><!-- .entry-utility -->
+		
+	</article>
+
+<?php endwhile; // End the loop. ?>
